58e2bec9f4c938a330b5aabecbec64aa3b0853c1
Gilson Peloso <gilson.peloso@webxnet.com.br>
2023-12-20 16:27:01 -0300

    nest generate service register, login, logout and generate controller auth --flat

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common'
+import { RegisterService } from './register/register.service'
+import { LoginService } from './login/login.service'
+import { LogoutService } from './logout/logout.service'
+import { AuthController } from './auth.controller'
 
-@Module({})
+@Module({
+    providers: [RegisterService, LoginService, LogoutService],
+    controllers: [AuthController],
+})
 export class AuthModule {}
