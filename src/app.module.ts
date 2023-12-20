e71d738f0176939046bdf35f958da9ccc8dfd695
Gilson Peloso <gilson.peloso@webxnet.com.br>
2023-12-20 15:59:32 -0300

    nest generate module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
 import { PrismaService } from './prisma/prisma.service'
 import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
     imports: [
@@ -24,6 +25,7 @@ import { UserModule } from './user/user.module'
             envFilePath: '.env',
         }),
         UserModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService, PrismaService],
