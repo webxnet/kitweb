e7f0e2816551ad5d8f73cfc6d9f63b62ebdc6b13
Gilson Peloso <gilson.peloso@webxnet.com.br>
2023-12-20 15:42:08 -0300

    nest generate module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
 import { PrismaService } from './prisma/prisma.service'
-import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module'
 
 @Module({
     imports: [
